c315cfb15dc6cacaaa154387941ca69ca2f3eed0
Emanuel Nieto <emanuelnieto@MacBook-Air.attlocal.net>
2021-12-02 21:44:34 -0600

    added third hour folder

---
+++ second-hour/main.js
@@ -0,0 +1,56 @@
+//declare Variable
+
+
+
+
+let btnDanger = true;
+const age = 26;
+
+age1= 15;
+
+console.log(btnDanger);
+
+//let lastName;
+
+//lastName = 'nieto'
+
+//console.log(lastName)
+
+const skill = "Manny's skills are slay"
+//why is my backslash blue and not working-
+const skill2 = 'Manny\'s skills are slay';
+
+console.log(skill2);
+
+const firstName = "Manny";
+const lastName = "Nieto";
+
+let fullName;
+
+fullName = firstName + ' ' + lastName;
+
+console.log('Hi, my full name is' + fullName)
+console.log(`HI, my full name is ${fullName}`);
+
+const website = 'google';
+
+const url = 'http://www.' + website + '.com';
+
+console.log(url);
+
+// let a = 5;
+// let b = 10;
+
+// age = 34;
+
+// console.log(`Fifteen is =+ ${age}`);
+
+let fname = 'manny'; //string
+let age = 24; //number
+let age3 = "46";
+let userProfile = true ; //boolean
+
+
+console.log(fname);
+console.log(age);
+console.log(age3);
