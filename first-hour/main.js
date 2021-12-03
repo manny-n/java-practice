2e7d2f9b7e02c2b5578112b14a880328af478ff0
Emanuel Nieto <emanuelnieto@MacBook-Air.attlocal.net>
2021-12-02 18:04:53 -0600

    added first hour

---
+++ first-hour/main.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.btn').forEach((item)=>{
+    item.addEventListener('click',()=>{
+        alert('manny is the awesomest best');
+    });
+});
+
+alert({name:'Elvis',
+        email: 'elvis@elvis.com'});
+
+document.write({name:"Elvis",
+        email: 'elvis@elvis.com'});
+
+console.log({name:'Elvis',
+        email: 'elvis@elvis.com'})  //the one we use a lot lol
